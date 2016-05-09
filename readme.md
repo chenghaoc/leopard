ae8a8a2e84a98690273c56ef096c10b38458ca5d
changbenny <changbenny0401@gmail.com>
2016-05-09 09:36:09 +0800

    list API, add readme

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Leopard
+
+60 fps pages made easy.
+
+Leopard is a performant, efficient,  heuristic scheduler for building user interface. Leopard eliminat janky experience by scheduling DOM related operation automatically.
+
+```javascript
+
+```
+
+### Installation
+
+### API
+
+### License
+
+MIT
--- schedule.js
+++ schedule.js
@@ -1,10 +1,43 @@
+var Emitter = function() {
+  this.uid = 0
+  this.handlers = []
+  for (let i = 0; i < 1000; i ++) this.handlers.push([])
+}
+Emitter.prototype.on = function(level, callback) {
+  this.handlers[level].push({
+    id: this.uid ++,
+    action: callback
+  })
+}
+Emitter.prototype.emit = function(level, ...args) {
+  this.handlers[level].forEach(handler => {
+    if (typeof(handler.action) === 'function')
+      handler.action(...args)
+  })
+}
+Emitter.prototype.once = function(level, callback) {
+  var id = this.uid
+  this.on(level, (...args) => {
+    callback(...args)
+    var handler = this.handlers[level].find(handler => handler.id === id)
+    this.handlers[level].splice(this.handlers[level].indexOf(handler), 1)
+    // delete handler
+  })
+}
+
+
+
 var rAF = requestAnimationFrame
 
 var Schedule = {}
 var queue = []
-var limit = 1500
+var counter = 0
+var limit = 10
+var balance = limit
+var goal = 17
 var isRunning = false
 var accelerate = 1 // for slow start
+var multiplier = 2
 
 var scriptStart
 var scriptEnd
@@ -13,50 +46,63 @@ var styleEnd
 
 var styleDuration
 var scriptDuration
+var emitter = new Emitter()
 
-for (let i = 0; i < 100; i ++) queue.push([])
+for (let i = 0; i < 1000; i ++) queue.push([])
 
 
 function frame(frameStart) {
   if (!isRunning) return
-  styleEnd = frameStart
+  // styleEnd = frameStart
 
+  
+  styleEnd = frameStart
   styleDuration = styleEnd - styleStart
   scriptDuration = scriptEnd - scriptStart
-
   scriptStart = now()
+
 
 
   // console.log(styleDuration)
   // console.log(scriptDuration)
-  if (scriptDuration < 17) {
-    accelerate = accelerate * 2
-    limit += accelerate * 10
-  }
-  else {
-    hit = true
-    accelerate = Math.floor(accelerate / 2)
-    limit = Math.floor(limit / 2)
-  }
-  console.log(limit)
+  // if (styleDuration < 17) {
+    // console.log(styleDuration);
+    console.log(styleDuration);
+    if (styleDuration < goal && styleDuration !== 0) {
+      accelerate = accelerate * 2
+      limit += accelerate
+      balance = limit
+    } else if (styleDuration >= goal) {
+      hit = true
+      accelerate = 1
+      limit = Math.floor(limit / 2)
+    } else {}
+  // } else {
+  //   limit = limit * multiplier
+  //   // multiplier = multiplier * 2
+  // }
   if (limit < 1)
     limit = 1
+  // console.log(accelerate);
+  // console.log(styleDuration);
   var count = limit
-  for (let i = 0; i < queue.length; i ++) {
+  for (var i = 0; i < queue.length; i ++) {
     if (count < 1) break
-    let level = queue[i]
+    var level = queue[i]
     while (level.length) {
       if (count < 1) break
+      counter --
       level.shift()()
+      if (!level.length)
+        emitter.emit(i)
       count --
     }
-    if (i === queue.length - 1)
+    if (i === queue.length - 1 && counter === 0)
       Schedule.stop()
   }
 
   scriptEnd = now()
-  styleStart = now()
-
+  styleStart = frameStart
   rAF(frame)
 }
 
@@ -64,6 +110,10 @@ function now() {
   return performance.now() || Date.now()
 }
 Schedule.stop = function() {
+  console.log('stop');
+  limit = balance
+  accelerate = 1 // for slow start
+  multiplier = 2
   isRunning = false
 }
 Schedule.start = function () {
@@ -75,8 +125,18 @@ Schedule.start = function () {
   rAF(frame)
 }
 Schedule.put = function(priority, callback, times) {
-  while (times--) 
+  if (!times) {
     queue[priority].push(callback)
+    counter ++
+  }
+  else {
+    while (times--)  {
+      queue[priority].push(callback)
+      counter ++
+    }
+  }
   if (!isRunning)
     Schedule.start()
 }
+Schedule.on = emitter.on.bind(emitter)
+Schedule.once = emitter.once.bind(emitter)
